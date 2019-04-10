acbf1cc7a01456d5eb8ce3edee627dd7302c5734
Filipe Graça <filipegraca1992@gmail.com>
2019-04-10 22:25:34 +0100

    moved folders

--- tutorial_02/worker.js
+++ tutorial_02/worker.js
@@ -7,9 +7,15 @@ rmq.connect("amqp://localhost", (err, conn) => {
   conn.createChannel((err, ch) => {
     const queue = "task_queue"
     ch.assertQueue(queue, {durable: true})
+    ch.prefetch(1)
     ch.consume(queue, (msg) => {
+      const secs = msg.content.toString().split(".").length - 1
       console.log("Received message from publisher: %s", msg.content.toString())
-    }, {noAck: true})
+      setTimeout(() => {
+        console.log("[x] Done")
+        ch.ack(msg)
+      }, secs * 1000)
+    }, {noAck: false})
   })
 
   setTimeout(() => {
